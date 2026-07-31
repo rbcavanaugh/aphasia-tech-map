f2860627ce9c8e58aab6555325b31602025cbca6
John E. Pierce <pierce.john.e@gmail.com>
2026-07-31 16:16:42 +1000

    Update to improve geolocation accuracy

--- R/getData.R
+++ R/getData.R
@@ -10,24 +10,61 @@ googlesheets4::gs4_deauth()
 
 sheet_url = 'https://docs.google.com/spreadsheets/d/1JKI9l_WxcO2nl7wrJ5wRvAy_PcezXPXalZ8s6Ae5CQY/edit?usp=sharing'
 
+collapse_locations = function(...) {
+  locations = c(...)
+  locations = stringr::str_squish(as.character(locations))
+  locations = locations[!is.na(locations) & locations != ""]
+
+  if (length(locations) == 0) {
+    return(NA_character_)
+  }
+
+  paste(locations, collapse = "; ")
+}
+
+normalise_geocode_location = function(location) {
+  location = stringr::str_squish(as.character(location))
+
+  dplyr::case_when(
+    stringr::str_detect(location, stringr::regex("^Hong Kong\\s+SAR,\\s*China$", ignore_case = TRUE)) ~ "Hong Kong, China",
+    TRUE ~ location
+  )
+}
+
 google_data = googlesheets4::read_sheet(sheet_url) %>%
   rowwise() %>%
   mutate(
-    geo = list(mb_geocode(LocationFull, limit = 3, access_token = token)),
-    geo = geo[1]
+    LocationDisplay = collapse_locations(
+      LocationFull,
+      LocationFull2,
+      `Other cities/countries of researchers:`
+    ),
+    LocationGeocode = normalise_geocode_location(LocationFull),
+    geo = list(
+      if (is.na(LocationGeocode) || LocationGeocode == "") {
+        c(NA_real_, NA_real_)
+      } else {
+        mb_geocode(LocationGeocode, limit = 1, access_token = token)
+      }
+    ),
+    lon = unname(unlist(geo))[1],
+    lat = unname(unlist(geo))[2]
     ) %>%
-  mutate(
-  lon = geo[1],
-  lat = geo[2]
-  ) %>%
   select(Timestamp,
          Email = "Email address",
          Title = "Project name",
          BriefSummary = "Aim of project:",
          ResponsiblePartyInvestigators = "Research team members",
-         LocationCountry = "Country where lead researchers are based:",
-         LocationCity = "City where lead researchers are based:",
+         LocationCity = "City of researchers (site 1)",
+         LocationRegion = "State/region/province of researchers (site 1)",
+         LocationCountry = "Country of researchers (site 1)",
          LocationFull,
+         LocationCity2 = "City of researchers (site 2)",
+         LocationRegion2 = "State/region/province of researchers (site 2)",
+         LocationCountry2 = "Country of researchers (site 2)",
+         LocationFull2,
+         OtherLocations = "Other cities/countries of researchers:",
+         LocationDisplay,
          Lat = lat,
          Lon = lon,
          Funded = "Is the project funded?",
@@ -41,5 +78,3 @@ google_data = googlesheets4::read_sheet(sheet_url) %>%
   )
 
 write_rds(google_data, file = "google_data.rds")
-
-
